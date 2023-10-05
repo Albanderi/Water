cd6ee48453fadaf6672d9cf0f747a7f24c10645c
Albanderi <Albanderi-bh@hotmail.com>
2023-10-05 12:33:52 +0300

    Initial Commit

---
+++ Water/WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApp.swift
+//  Water
+//
+//  Created by Albanderi Binhasher on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
